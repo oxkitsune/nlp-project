409d36a3571ff8398081d05bf00f926970d10331
Gijs de Jong <berichtaangijs@gmail.com>
2021-04-25 17:29:07 +0200

    Add basic statistics to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,29 @@
 import nltk
 from nltk import word_tokenize
+from collections import Counter
 
 corpus = ''
 
+total_lines = 0
+total_letters = 0
+
 with open('corpus.txt', 'r')  as file:
-    corpus = "".join([line for line in file.readlines()])
+    lines = file.readlines()
+    total_lines = len(lines)
+    corpus = "".join([line for line in lines if not line.startswith('#')])
 
 
-tokens = word_tokenize(corpus)
-print(tokens)
+tokens = word_tokenize(corpus.strip())
+for token in tokens:
+    total_letters += len(token)
 
-# print([line.lower() for line in corpus.split('.') if line != ''])
+counter = Counter(tokens)
+
+# print(tokens)
+
+print(f'Total Lines: {total_lines}')
+print(f'Total Word Count: {len(tokens)}')
+print(f'Total Letters: {total_letters}')
+print(f'Avg. Words per Line:  {len(tokens)/total_lines}')
+print(f'Avg. Letters per Word: {total_letters/len(tokens)}')
+print(f'Most Common Token: "{counter.most_common(1)[0][0]}" with {counter.most_common(1)[0][1]} occurences')
